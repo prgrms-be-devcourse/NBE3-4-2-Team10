7415d53717c43fae911f578160223f03da1d4bf1
inwoo <dlsdn1108@gmail.com>
2025-02-04 17:30:59 +0900

    feat: 회원 탈퇴 백엔드, 쿠키 관련 테스트 코드 수정 (#22)
    * feat: 사용자 탈퇴 백엔드
    
    * fix: 테스트 코드 쿠키 설정 수정
    
    * refactor: 구조 개선
    
    * feat: 삭제 날짜 추가 (로그나 하드 삭제 위해서)
    
    * fix: 회원조회시 탈퇴하지 않은 사용자만 조회

--- backend/src/main/java/com/ll/TeamProject/domain/user/controller/UserController.java
+++ backend/src/main/java/com/ll/TeamProject/domain/user/controller/UserController.java
@@ -2,14 +2,14 @@ package com.ll.TeamProject.domain.user.controller;
 
 import com.ll.TeamProject.domain.user.dto.UserDto;
 import com.ll.TeamProject.domain.user.entity.SiteUser;
+import com.ll.TeamProject.domain.user.service.UserService;
 import com.ll.TeamProject.global.rq.Rq;
+import com.ll.TeamProject.global.rsData.RsData;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SecurityRequirement(name = "bearerAuth")
 public class UserController {
     private final Rq rq;
+    private final UserService userService;
 
     @GetMapping("/me")
     @Operation(summary = "내 정보")
@@ -25,6 +26,16 @@ public class UserController {
         SiteUser actor = rq.findByActor().get();
 
         return new UserDto(actor);
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "회원 탈퇴 (soft)")
+    public RsData<UserDto> deleteUser(@PathVariable("id") long id) {
+        SiteUser userToDelete = userService.delete(id);
+
+        return new RsData<>(
+                "200-1", "회원정보가 삭제되었습니다.", new UserDto(userToDelete)
+        );
     }
 
 }
