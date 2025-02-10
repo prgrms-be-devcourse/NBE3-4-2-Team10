0cfb795e78471fdb1c221bac9e86dd236f611f1f
kwonkiyong0059 <kkoung425@gmail.com>
2025-02-10 17:57:18 +0900

    fix: [backend] 사용자 인증로직 추가

--- backend/src/test/java/com/ll/TeamProject/domain/schedule/ScheduleControllerTest.java
+++ backend/src/test/java/com/ll/TeamProject/domain/schedule/ScheduleControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -31,9 +32,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
-@Sql(scripts = "/reset-db.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD) // ✅ 테스트 실행 전 DB 초기화
+@ActiveProfiles("test")
+@Sql(scripts = "/reset-db.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD) // 테스트 실행 전 DB 초기화
 public class ScheduleControllerTest {
-
 
     @Autowired
     private ScheduleRepository scheduleRepository;
@@ -52,7 +53,6 @@ public class ScheduleControllerTest {
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
         scheduleRepository.deleteAll();
-
 
     }
 
