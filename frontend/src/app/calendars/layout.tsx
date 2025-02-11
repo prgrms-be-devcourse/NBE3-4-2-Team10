4413e6fd4ba1ae567b4da61c638d048fe387720a
c-hano <wdq0304@naver.com>
2025-02-11 12:35:11 +0900

    merge: develop

--- frontend/src/app/calendars/layout.tsx
+++ frontend/src/app/calendars/layout.tsx
@@ -4,8 +4,6 @@ import {useRouter} from "next/navigation";
 import Script from "next/script";
 import {useEffect, useState} from "react";
 
-//  로그아웃로직에 JSESSIONID 를추가로제거하는게필요함(세션에도인증정보가들어있음)
-//  아니면로그인시에세션에인증정보를넣지말아야함.
 
 export const LoginRequired = <T,>(WrappedComponent: React.FC<T>) => {
     const InnerFunction: React.FC<T> = (props: T) => {
@@ -13,7 +11,7 @@ export const LoginRequired = <T,>(WrappedComponent: React.FC<T>) => {
         const [loaded, setLoaded] = useState(false);
         useEffect(() => {
             client
-                .GET("/user/me")
+                .GET("/api/user/me")
                 .then(({data}) => {
                     if (data?.id) {
                         console.log(data);
@@ -43,10 +41,6 @@ export default function CalendarLayout({
             <Script
                 src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js"
                 strategy="afterInteractive"
-            />
-            <link
-                href="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/main.min.css"
-                rel="stylesheet"
             />
             {children}
         </div>
