838d151385465ad5eb33a3586f5f4359cf1b4a44
kwonkiyong0059 <kkoung425@gmail.com>
2025-02-24 01:05:54 +0900

    refactor: ScheduleRepository 개선(Named Query 활용)

--- backend/src/main/java/com/ll/TeamProject/domain/schedule/repository/ScheduleRepository.java
+++ backend/src/main/java/com/ll/TeamProject/domain/schedule/repository/ScheduleRepository.java
@@ -2,7 +2,6 @@ package com.ll.TeamProject.domain.schedule.repository;
 
 import com.ll.TeamProject.domain.schedule.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
@@ -11,23 +10,15 @@ import java.util.Optional;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
-    // 특정 캘린더에서 겹치는 일정 조회
-    @Query("""
-    SELECT s FROM Schedule s WHERE s.calendar.id = :calendarId AND\s
-    (s.startTime < :endTime AND s.endTime > :startTime)
-   \s""")
+    // NamedQuery "Schedule.findOverlappingSchedules"를 사용
     List<Schedule> findOverlappingSchedules(@Param("calendarId") Long calendarId,
                                             @Param("startTime") LocalDateTime startTime,
                                             @Param("endTime") LocalDateTime endTime);
 
-
-    // 특정 캘린더 내 일정 조회
-    @Query("SELECT s FROM Schedule s WHERE s.calendar.id = :calendarId AND " +
-            "(s.startTime < :endDate AND s.endTime > :startDate)")
+    // NamedQuery "Schedule.findSchedulesByCalendarAndDateRange"를 사용
     List<Schedule> findSchedulesByCalendarAndDateRange(@Param("calendarId") Long calendarId,
                                                        @Param("startDate") LocalDateTime startDate,
                                                        @Param("endDate") LocalDateTime endDate);
-
 
     Optional<Schedule> findTopByOrderByIdDesc();
 }
