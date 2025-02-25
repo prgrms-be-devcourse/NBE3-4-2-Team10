b0fb59e31e5512c86600de1cb80865079e8df185
Taeheeeeeee <taeheemin62@gmail.com>
2025-02-25 13:16:52 +0900

    refactor: 접근제한 프론트 수정

--- frontend/src/app/admin/admins/list/page.tsx
+++ frontend/src/app/admin/admins/list/page.tsx
@@ -33,6 +33,23 @@ export default async function Page({
       cookie: (await cookies()).toString(),
     },
   });
+  if (response.response.status === 403) {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="bg-gray-30 p-6 rounded-lg shadow-lg max-w-lg w-full text-center">
+          <h2 className="text-3xl font-bold text-red-600 mb-4">
+            !!! 권한이 없습니다. !!!
+          </h2>
+          <hr className="border-t-2 border-red-600 mb-4" />
+          <p className="text-xl text-gray-700">
+            이 페이지에 접근할 수 있는 권한이 없습니다.
+          </p>
+          <p className="mt-4 text-gray-500">관리자에게 문의하세요.</p>
+        </div>
+      </div>
+    );
+  }
+
   const itemPage = response.data?.data;
   return (
     <>
