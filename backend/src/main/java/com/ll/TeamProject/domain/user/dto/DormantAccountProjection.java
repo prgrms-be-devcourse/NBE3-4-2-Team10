515b411b351b5d6bcbc957f1bd3fda9991b95f71
inwoo <dlsdn1108@gmail.com>
2025-02-27 17:53:29 +0900

    refactor: 쿼리 최적화

--- backend/src/main/java/com/ll/TeamProject/domain/user/dto/DormantAccountProjection.java
+++ backend/src/main/java/com/ll/TeamProject/domain/user/dto/DormantAccountProjection.java
@@ -1,10 +1,6 @@
 package com.ll.TeamProject.domain.user.dto;
 
-import java.time.LocalDateTime;
-
 public interface DormantAccountProjection {
-    Long getId();
     String getNickname();
     String getEmail();
-    LocalDateTime getLastLogin();
 }
